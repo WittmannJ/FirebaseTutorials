2a3c0a45b04632629a33ce4324891fc49609e23c
Jakob Wittmann <jacklerGM@web.de>
2018-10-10 21:25:43 +0200

    clean-up before data retrieval

--- app/src/main/java/com/wittmann/jackl/firebasetutorials/MainActivity.java
+++ app/src/main/java/com/wittmann/jackl/firebasetutorials/MainActivity.java
@@ -17,9 +17,7 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     // tutorial: https://www.youtube.com/watch?v=tg_7eV99RfM&list=PLGCjwl1RrtcSi2oV5caEVScjkM6r3HO9t&index=6
-    private Button mFirebaseBtn;
-    private EditText mNameField;
-    private EditText mEmailField;
+
 
     private DatabaseReference mDatabase;
 
@@ -28,39 +26,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        mNameField = (EditText) findViewById(R.id.name_field);
-        mEmailField = (EditText) findViewById(R.id.email_field);
 
-        mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // 1 - Create Child in root object of database
-                // 2 - Assign some value to the child object
-
-                String name = mNameField.getText().toString().trim();
-                String email = mEmailField.getText().toString().trim();
-
-                HashMap<String, String> dataMap = new HashMap<>();
-                dataMap.put("Name", name);
-                dataMap.put("Email", email);
-
-                mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, "Stored...", Toast.LENGTH_LONG).show();
-                        }
-                        else {
-                            Toast.makeText(MainActivity.this, "Error...", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-
-            }
-        });
     }
 }
