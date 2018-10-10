2d784cbf475a3049e0f456f867bcfdc50b46a420
Jakob Wittmann <jacklerGM@web.de>
2018-10-10 21:44:32 +0200

    my perfect attempt at retrieving multiple data

--- app/src/main/java/com/wittmann/jackl/firebasetutorials/MainActivity.java
+++ app/src/main/java/com/wittmann/jackl/firebasetutorials/MainActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private DatabaseReference mDatabase;
+    private TextView mNameView;
+    private TextView mAgeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mNameView = (TextView) findViewById(R.id.name_view);
+        mAgeView = (TextView) findViewById(R.id.age_view);
 
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String name = dataSnapshot.child("Name").getValue().toString();
+                String age = dataSnapshot.child("Age").getValue().toString();
+
+                mNameView.setText("Name : " + name);
+                mAgeView.setText("Age : " + age);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
