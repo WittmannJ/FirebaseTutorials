c284b6e067d4e159db2beba7d3f5d9e04554616c
Jakob Wittmann <jacklerGM@web.de>
2018-10-10 21:20:53 +0200

    first implementation of sending data to google

--- app/src/main/java/com/wittmann/jackl/firebasetutorials/MainActivity.java
+++ app/src/main/java/com/wittmann/jackl/firebasetutorials/MainActivity.java
@@ -1,12 +1,27 @@
 package com.wittmann.jackl.firebasetutorials;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     // tutorial: https://www.youtube.com/watch?v=tg_7eV99RfM&list=PLGCjwl1RrtcSi2oV5caEVScjkM6r3HO9t&index=6
     private Button mFirebaseBtn;
+    private EditText mNameField;
+    private EditText mEmailField;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +29,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mNameField = (EditText) findViewById(R.id.name_field);
+        mEmailField = (EditText) findViewById(R.id.email_field);
+
+        mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 1 - Create Child in root object of database
+                // 2 - Assign some value to the child object
+
+                String name = mNameField.getText().toString().trim();
+                String email = mEmailField.getText().toString().trim();
+
+                HashMap<String, String> dataMap = new HashMap<>();
+                dataMap.put("Name", name);
+                dataMap.put("Email", email);
+
+                mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Stored...", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Error...", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
